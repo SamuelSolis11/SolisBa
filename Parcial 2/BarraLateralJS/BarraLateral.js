b2b113653b1a084812875854e2c9f1f3a23c3514
SamuelSolis11 <samuelsolis1183@gmail.com>
2022-03-29 12:26:56 -0600

    Agregue BarraLateralJS

---
+++ Parcial 2/BarraLateralJS/BarraLateral.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click",alternarMenu);
+function alternarMenu(){
+    //alert("Samuel")
+    document.getElementById("sb").classList.toggle("MenuEscondido");
+    document.getElementById("ct").classList.toggle("ContenidoMargenCero");
+}
