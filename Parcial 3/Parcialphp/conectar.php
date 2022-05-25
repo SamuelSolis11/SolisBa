09f3b9af444b348dd99f458cdf7a018154823d8e
SamuelSolis11 <samuelsolis1183@gmail.com>
2022-05-25 12:56:38 -0500

    Actualize algo

--- Parcial 3/Parcialphp/conectar.php
+++ Parcial 3/Parcialphp/conectar.php
@@ -1,4 +1,6 @@
 <?php
+$ID_Jugador=5;
+
 
    $hostname = 'localhost' ;
     $database = 'L19100257' ;
@@ -14,4 +16,18 @@
   }
 
   $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+  try{
+    $sql = "select * from Jugador where ID_Jugador=".$ID_Jugador;
+    $query = $con->prepare($sql);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    $query->closeCursor();
+}catch(PDOException $e) {
+    echo "Error de consulta a la base de datos";
+    echo $e->getMessage();
+}
+
+echo json_encode($result);
+
  ?>
